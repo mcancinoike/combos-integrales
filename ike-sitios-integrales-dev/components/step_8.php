af4c85646d3255fb4cdd49a683a28e26e6d52e09
jmsoberanes <jcortes@ikeasistencia.com>
2024-10-30 12:17:25 -0600

    Fix cambio de texto vista tarjeta de debito

--- ike-sitios-integrales-dev/components/step_8.php
+++ ike-sitios-integrales-dev/components/step_8.php
@@ -18,7 +18,7 @@
 	<div class="info">
 		<div class="box">
 			<div class="box__title">
-				Para finalizar, ingresa el número de tu Tarjeta de Crédito o Débito HSBC. Todos tus datos se encuentran protegidos.
+				Para finalizar, ingresa el número de Tarjeta de Crédito HSBC donde se domiciliará el pago o Débito HSBC donde se realizarán los cargos recurrentes. Todos los datos se encuentran protegidos
 			</div>
 		</div>
 
