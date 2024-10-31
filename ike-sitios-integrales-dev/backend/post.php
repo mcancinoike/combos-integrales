71386115d5bba91941e40a66a4e71a8a790d135a
Mauricio Cancino Ramírez <mcancino@ikeasistencia.com>
2024-10-31 09:53:12 -0600

    fix NaN subtotal

--- ike-sitios-integrales-dev/backend/post.php
+++ ike-sitios-integrales-dev/backend/post.php
@@ -3,12 +3,22 @@ require_once "../backend/conexion/conexion.php";
 $conexion = new conexion;
 
 $suma_asegurada = !isset($_POST['suma_asegurada']) ? "0.00" : $conexion->xssClean($_POST['suma_asegurada']);
+$suma_asegurada =  empty($suma_asegurada) ? 0 : $suma_asegurada;
+
 $prima_anual = !isset($_POST['prima_anual']) ? "0.00" : $conexion->xssClean($_POST['prima_anual']);
+$prima_anual = empty($prima_anual) ? 0 : $prima_anual;
+
 $subtotal_mensual = !isset($_POST['subtotal_mensual']) ? "0.00" : $conexion->xssClean($_POST['subtotal_mensual']);
+$subtotal_mensual = empty($subtotal_mensual) ? 0 : $subtotal_mensual;
+
 $subtotal_mensual_asistencia = !isset($_POST['subtotal_mensual_asistencia']) ? $subtotal_mensual : $conexion->xssClean($_POST['subtotal_mensual_asistencia']);
+
 $idPrima = !isset($_POST['idPrima']) ? 0 : $conexion->xssClean($_POST['idPrima']);
+
 $asistencias = !isset($_POST['asistencias']) ? [] : $_POST['asistencias'];
+
 $idCliente = !isset($_POST['idCliente']) ? 0 : $conexion->xssClean($_POST['idCliente']);
+
 $textAsistencia = "";
 foreach ($asistencias as $val) {
     $textAsistencia .= $conexion->xssClean($val) . "|";
